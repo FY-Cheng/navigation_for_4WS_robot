c4fb68572d2b62ea1025a69da2ad7f8371fae75f
FY-Cheng <18093121784@163.com>
2026-04-23 17:23:07 +0800

    nav2配置，地图和定位部分正常了

--- robot_navigation/launch/navigation2.launch.py
+++ robot_navigation/launch/navigation2.launch.py
@@ -35,14 +35,40 @@ def generate_launch_description():
     )
 
 
+    # nav2_bringup 禁用了 'use_localization' 后，map_server也会被禁用，所以在此单独启动
+    map_server_node = Node(
+        package='nav2_map_server',
+        executable='map_server',
+        name='map_server',
+        output='screen',
+        parameters=[{
+            'use_sim_time': use_sim_time,
+            'yaml_filename': PathJoinSubstitution([get_package_share_directory(nav_package), "map", map_file])
+        }]
+    )
+
+    # 这是核心！负责激活 map_server
+    lifecycle_manager_node = Node(
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager_map',
+        output='screen',
+        parameters=[
+            {'use_sim_time': use_sim_time,
+             'autostart': True,  # 自动激活节点
+             'node_names': ['map_server']}  # 管理的节点
+        ]
+    )
+
+
     nav2_bringup_dir = os.path.join(get_package_share_directory('nav2_bringup'), 'launch')
     nav2_bringup = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([nav2_bringup_dir, '/bringup_launch.py']),
         launch_arguments={
             'use_sim_time': use_sim_time,
-            'map': PathJoinSubstitution([get_package_share_directory(nav_package), "map", map_file]),
+            # 'map': PathJoinSubstitution([get_package_share_directory(nav_package), "map", map_file]),
             'params_file': PathJoinSubstitution([get_package_share_directory(nav_package), "param", params_file]),
-            'use_amcl': 'false',
+            'use_localization': 'False' # 禁用 localization_launch.py，从而禁用 AMCL，通过外部定位节点提供位姿信息。但需要单独启动地图服务器
         }.items()
     )
 
@@ -63,6 +89,8 @@ def generate_launch_description():
     ld.add_action(map_arg)
     ld.add_action(params_arg)
     ld.add_action(nav2_bringup)
+    ld.add_action(map_server_node)
+    ld.add_action(lifecycle_manager_node)
     ld.add_action(rviz_node)
 
     return ld
